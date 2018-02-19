b6c9340e86a6b7d796803a313769f81f5096d8f2
jesse <jessedp@gmail.com>
2018-02-19 14:05:33 -0500

    add conversion helpers

--- src/Timezones/Timezones.php
+++ src/Timezones/Timezones.php
@@ -201,4 +201,47 @@ class Timezones
 
         return $list;
     }
+
+
+    /**
+     * @param integer $timestamp
+     * @param string $timezone
+     * @param string $format
+     *
+     * @return string
+     */
+    public function convertFromUTC($timestamp, $timezone, $format = 'Y-m-d H:i:s (e)')
+    {
+        $date = new DateTime($timestamp, new DateTimeZone('UTC'));
+
+        $list = DateTimeZone::listIdentifiers();
+        if (!in_array($timezone, $list)){
+            $timezone = 'UTC';
+        }
+        $date->setTimezone(new DateTimeZone($timezone));
+
+        return $date->format($format);
+    }
+
+    /**
+     * Convert a timestamp to UTC
+     * @param integer $timestamp
+     * @param string $timezone
+     * @param string $format
+     *
+     * @return string
+     */
+    public function convertToUTC($timestamp, $timezone, $format = 'Y-m-d H:i:s (e)')
+    {
+        $list = DateTimeZone::listIdentifiers();
+        if (!in_array($timezone, $list)) {
+            $timezone = 'UTC';
+        }
+
+        $date = new DateTime($timestamp, new DateTimeZone($timezone));
+
+        $date->setTimezone(new DateTimeZone('UTC'));
+
+        return $date->format($format);
+    }
 }
