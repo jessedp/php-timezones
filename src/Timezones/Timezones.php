e4b0c66dab0ddc8b5fbcd5a23bc8a614fdd8c029
jesse <jessedp@gmail.com>
2018-02-19 13:18:23 -0500

    default to not grouping by region, fix sorting & labels

--- src/Timezones/Timezones.php
+++ src/Timezones/Timezones.php
@@ -58,80 +58,113 @@ class Timezones
     {
         $time = new DateTime(null, new DateTimeZone($timezone));
         $str_offset = $time->format('P');
-        $offset = str_replace('-', ' &minus; ', $str_offset);
-        $offset = str_replace('+', ' &plus; ', $str_offset);
 
-        $timezone = substr($timezone, strlen($region) + 1);
+        //clean up the html display
+        $signs = ['-','+'];
+        $signs_r = [' &minus; ',' &plus; '];
+        $offset = str_replace( $signs, $signs_r, $str_offset);
+
+        //do this for sorting later...
+        $dbl_offset = (double)str_replace(':','.', $str_offset);
+
+        //only strip things if we're bothering with regions
+        if (!empty($region)) {
+            $timezone = substr($timezone, strlen($region) + 1);
+        }
+
         $timezone = str_replace('St_', 'St. ', $timezone);
         $timezone = str_replace('_', ' ', $timezone);
 
         $formatted = '(GMT/UTC' . $offset . ')' . self::WHITESPACE_SEP . $timezone;
-        return ['offset'=>$str_offset, 'label' => $formatted];
+        return ['offset'=>$dbl_offset, 'label' => $formatted];
     }
 
     /**
      * Create a timezone HTML select element for form
      *
      * @param string $name the name/id to be used for the element
-     * @param array $opts various options to set, including:
-     *      @subparam string $selected the timezone name
-     *      @subparam mixed $attr key=>value pairs of attributes to apply
      * @param string $selected
-     * @param mixed $attr
+     * @param array $opts various options to set, including:
+     *      @subparam array $attr key=>value pairs of attributes to apply to the select element
+     *      @subparam bool $with_regions whether or not to do region grouping (default=false)
      * @return string
      **/
-    public function create($name, $selected='', $attr='')
+    public function create($name, $selected='', $opts=[])
     {
+        
         // Attributes for select element
-        $attrSet = null;
-        if (!empty($attr)) {
-            if (is_array($attr)) {
-                foreach ($attr as $attr_name => $attr_value) {
-                    $attrSet .= ' ' .$attr_name. '="' .$attr_value. '"';
-                }
-            } else {
-                $attrSet = ' ' .$attr;
+        $attrSet = '';
+        if (isset($opts['attr']) && is_array($opts['attr']) && !empty($opts['attr'])){
+            $attr = $opts['attr'];
+
+            foreach ($attr as $attr_name => $attr_value) {
+                $attrSet .= ' ' .$attr_name. '="' .$attr_value. '"';
             }
+        }
+
+        //setup grouping
+        $with_regions = false;
+        if (isset($opts['with_regions']) && is_bool($opts['with_regions']) && $opts['with_regions']){
+            $with_regions = true;
         }
 
         // start select element
         $listbox = '<select name="' .$name. '"' .$attrSet. '>';
 
-        // Add popular timezones
-        $listbox .= '<optgroup label="General">';
-        foreach ($this->popularTimezones as $key => $value) {
-            $selected_attr = ($selected == $key) ? ' selected="selected"' : '';
-            $listbox .= '<option value="' .$key. '" ' .$selected_attr. '>' .$value. '</option>';
+        if ($with_regions) {
+            // Add popular timezones
+            $listbox .= '<optgroup label="General">';
+            foreach ($this->popularTimezones as $key => $value) {
+                $selected_attr = ($selected == $key) ? ' selected="selected"' : '';
+                $listbox .= '<option value="' . $key . '" ' . $selected_attr . '>' . $value . '</option>';
+            }
+            $listbox .= '</optgroup>';
         }
-        $listbox .= '</optgroup>';
 
-        // Add all timezone of the regions
-        foreach ($this->regions as $continent => $mask) {
+        if ($with_regions){
+            $regions = $this->regions;
+        } else {
+            $regions = ['All'=>DateTimeZone::ALL];
+        }
+        // Add all timezones of the regions
+        // depending on with_regions, this may be one or muiltiple loops
+        foreach ($regions as $continent => $mask) {
             $opts = [];
             $timezones = DateTimeZone::listIdentifiers($mask);
 
-            // start optgroup tag
-            $listbox .= '<optgroup label="' .$continent. '">';
+            if ($with_regions) {
+                // start optgroup tag
+                $listbox .= '<optgroup label="' . $continent . '">';
+            } else {
+                //when including everything, don't let formatTimeZone truncate the label.
+                $continent = null;
+            }
 
             // create option tags
+            $offsets = [];
+            $labels = [];
             foreach ($timezones as $timezone) {
                 $opt = $this->formatTimezone($timezone, $continent);
                 $opt['tz'] = $timezone;
                 $opt['selected'] = ($selected == $timezone) ? ' selected="selected"' : '';
+                $offsets[$timezone] = $opt['offset'];
+                $labels[$timezone] = $opt['label'];
                 $opts[] = $opt;
             }
 
-            array_multisort(array_column($opts, 'offset'), SORT_ASC,
-                            array_column($opts, 'label'), SORT_ASC,
-                            $opts);
+            array_multisort($offsets, SORT_DESC,
+                            $labels, SORT_ASC, $opts
+                            );
 
             foreach($opts as $opt){
                 $listbox .= '<option value="' . $opt['tz'] . '"' . $opt['selected'] . '>';
                 $listbox .= $opt['label'];
                 $listbox .= '</option>';
             }
-            // end optgroup tag
-            $listbox .= '</optgroup>';
+            if ($with_regions) {
+                // end optgroup tag
+                $listbox .= '</optgroup>';
+            }
         }
 
         // end select element
